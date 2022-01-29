85a5a046802a1e721b804ebf1bd3e24a887f80bc
woawo1213 <woawo1213@gmail.com>
2022-01-29 22:13:37 +0900

    upload

--- tutorial.cpp
+++ tutorial.cpp
@@ -131,36 +131,156 @@ int main()
         cout << "######################fill & memset######################" << endl;
         cout << endl;
         // vector 초기화 하기
-        int n = 10;
-        vector<int> v[10];   // v벡터를 10개를 생성합니다.
-        vector<int> v(n, 0); // 0이라는 value를 가진 n개의 벡터를 생성
-        // vector로 2차원 배열 매트릭스 만들기
-        vector<vector<int>> checked;
-        vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
-        // fill로도 초기화할 수 있다.
-        fill(v.begin(), b.end(), 0);
-        //배열초기화 shortcut : 전체 0으로 초기화한다. 일부 컴파일러에서 통하지 않을 수도있습니다.
-        int dp[10] = {
-            0,
-        };
-        int dp[10][10] = {{
-            0,
-        }};
-        fill(dist, dist + MAX_N, 0);
-        //부분초기화 : 0번째를 0, 1번째를 1로 초기화한다.
-        int a[n] = {0, 1};
-        memset(check, 0, sizeof(check));
+        // int n = 10;
+        // vector<int> v1[10];   // v벡터를 10개를 생성합니다.
+        // vector<int> v2(n, 0); // 0이라는 value를 가진 n개의 벡터를 생성
+        // // vector로 2차원 배열 매트릭스 만들기
+        // vector<vector<int>> checked;
+        // vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
+        // // fill로도 초기화할 수 있다.
+        // fill(v.begin(), v.end(), 0);
+        // //배열초기화 shortcut : 전체 0으로 초기화한다. 일부 컴파일러에서 통하지 않을 수도있습니다.
+        // // int dp[10] = {
+        // //     0,
+        // // };
+        // // int dp[10][10] = {{
+        // //     0,
+        // // }};
+        // // fill(dist, dist + MAX_N, 0);
+        // //부분초기화 : 0번째를 0, 1번째를 1로 초기화한다.
+        // int a[n] = {0, 1};
+        // memset(check, 0, sizeof(check));
         // fill을 사용한 2차원 배열 초기화는 이렇습니다.
-        for (int i = 0; i < 10; i++)
-            fill(dp[i], dp[i] + 10, 0);
-        fill(&arr[0][0], &arr[0][0] + n * m, k) // 이런식으로 한번에 초기화를 할 수 있습니다.
+        // for (int i = 0; i < 10; i++)
+            // fill(dp[i], dp[i] + 10, 0);
+        // fill(&arr[0][0], &arr[0][0] + n * m, k); // 이런식으로 한번에 초기화를 할 수 있습니다.
+    
+        //배열복사
+        // memcpy(temp,a ,sizeof(a));
     }
 
+    {
+        /* map */
+        cout << "######################map######################" << endl;
+        cout << endl;
+        //레드-블랙 트리구조 내장
+        // 데이터 삽입 시 정렬되어 삽입 map
+        // 정렬 안된 맵 삽입시 unordered_map
+        int v[10];
+        unordered_map<string,int>umap;
+        umap.insert({"test1",1});
+        umap.emplace("test4",4);
+        umap["test2"]=2;//권장 방법
+
+        for(auto element:umap){
+            cout<<element.first<<" : "<<element.second<<'\n';
+        }
+        auto search =umap.find("test4");
+        if(search!=umap.end()){
+            cout<<"found: "<<search->first<<" "<<(*search).second<<'\n';
+        }else{
+            cout<<"not found"<<'\n';
+        }
+
+        umap["test1"]++;
+        cout << umap["test1"] << "\n";
+        cout <<umap.size() <<"\n";
+        umap.erase("test1");
+        cout <<umap.size() <<"\n";
+
+        //map 순환시 key=first value=second로 탐색
+        map<string, int> _map;
+        _map["큰돌"]++;
+        _map["큰돌"]++;
+        for(auto c : _map){
+            cout << c.first << " : " << c.second <<"\n";
+        }
+    }
+
+    {
+        /* set */
+        cout << "######################set######################" << endl;
+        cout << endl;
+        //중복제거, pair로저장 가능하며 first second로 접근가능
+        set<pair<string, int>> _set;
+        _set.insert({"test", 1});
+        _set.insert({"test", 1});
+        _set.insert({"test", 1});
+        _set.insert({"test", 1});
+        cout <<_set.size() <<"\n";
+    }
+    {
+        /* multiset */
+        cout << "######################multiset######################" << endl;
+        cout << endl;
+        //중복가능, key value 상관없음 알아서 들어감
+        multiset<int> s;
+        s.insert(12);
+        s.insert(10);
+        s.insert(2);
+        s.insert(10);
+        s.insert(90);
+        s.insert(85);
+        s.insert(45);
+        cout << "Multiset elements after sort\n";
+        for (auto it = s.begin(); it != s.end(); it++)
+            cout << *it << ' ';
+        cout << '\n';
+        auto it1 = s.find(10);
+        auto it2 = s.find(90);
+        // elements from 10 to elements before 90
+        // erased
+        s.erase(it1, it2);
+        cout << "Multiset Elements after erase:\n";
+        for (auto it = s.begin(); it != s.end(); it++)
+            cout << (*it) << ' ';
+        s.erase(s.begin());
+        cout << "\n";
+        for (auto it = s.begin(); it != s.end(); it++)
+            cout << (*it) << ' ';
+    }
     {
         /* algo */
         cout << "######################algo######################" << endl;
         cout << endl;
     }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+    {
+        /* algo */
+        cout << "######################algo######################" << endl;
+        cout << endl;
+    }
+
+
 
     return 0;
 }
