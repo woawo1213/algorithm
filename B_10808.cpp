553b1d1555045b13ed33e83b0c370897ea777305
woawo1213 <woawo1213@gmail.com>
2022-02-25 00:07:43 +0900

    up

--- B_10808.cpp
+++ B_10808.cpp
@@ -1,20 +1,34 @@
+// counting array
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-string str;
-int cnt[26];
+int alphabet[26];
+string word;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> str;
-    for (char a : str)
-    {
-        cnt[a - 'a']++;
-    }
-    for (int i = 0; i < 26; i++)
-        cout << cnt[i] << " ";
 
+    cin >> word;
+
+    for (int i = 0; i < word.length(); i++)
+    {
+        if (word[i] >= 'a' && word[i] <= 'z')
+        {
+            alphabet[word[i] - 'a']++;
+        }
+    }
+
+    /* better way
+    for (char c : word)
+    {
+        alphabet[c - 'a']++;
+    }
+    */
+
+    for (int i = 0; i < 26; i++)
+    {
+        cout << alphabet[i] << " ";
+    }
     return 0;
 }
