2c9edbb714f9bb33e3f0b9269d28d6a4d78e1c96
woawo1213 <woawo1213@gmail.com>
2022-02-28 11:35:51 +0900

    up

--- B_1159.cpp
+++ B_1159.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, cnt[26];
-string s, ret;
+int people, cnt[26];
+string name, ret;
 int main()
 {
-    cin >> n;
-    for (int i = 0; i < n; i++)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> people;
+    for (int i = 0; i < people; i++)
     {
-        cin >> s;
-        cnt[s[0] - 'a']++;
+        cin >> name;
+        cnt[name[0] - 97]++;
     }
     for (int i = 0; i < 26; i++)
+    {
         if (cnt[i] >= 5)
             ret += i + 'a';
+    }
     if (ret.size())
-        cout << ret << "\n";
+        cout << ret << endl;
     else
-        cout << "PREDAJA"
-             << "\n";
+        cout << "PREDAJA" << endl;
+
+    return 0;
 }
--- B_11655.cpp
+++ B_11655.cpp
@@ -3,18 +3,22 @@ using namespace std;
 string s;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     getline(cin, s);
+
     for (int i = 0; i < s.size(); i++)
     {
-        // 대문자인경우
-        if (s[i] >= 65 && s[i] < 97)
+        if (s[i] >= 65 && s[i] < 97) // 대문자
         {
             if (s[i] + 13 > 90)
                 s[i] = s[i] + 13 - 26;
             else
                 s[i] = s[i] + 13;
         }
-        else if (s[i] >= 97 && s[i] <= 122)
+        else if (s[i] >= 97 && s[i] <= 122) // 소문자
         {
             if (s[i] + 13 > 122)
                 s[i] = s[i] + 13 - 26;
