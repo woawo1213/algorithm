46fdb81918b90725397c9fd196d1b23ca3849d9d
woawo1213 <woawo1213@gmail.com>
2022-03-01 23:35:59 +0900

    up

--- B_9996.cpp
+++ B_9996.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int cnt, arr[104];
-string pattern, s;
+string pattern, s, pre, suf;
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -11,17 +11,28 @@ int main()
     cin >> cnt;
     cin >> pattern;
 
-    for (int i = 0; i < cnt; i++)
-    {
-        cin >> s;
-    }
+    int pos = pattern.find('*');
+    pre = pattern.substr(0, pos);
+    suf = pattern.substr(pos + 1);
 
     for (int i = 0; i < cnt; i++)
     {
-        if (arr[i] == 1)
-            cout << "DA" << endl;
-        else
+        cin >> s;
+        if (pre.size() + suf.size() > s.size())
+        {
             cout << "NE" << endl;
+        }
+        else
+        {
+            if (pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size()))
+            {
+                cout << "DA" << endl;
+            }
+            else
+            {
+                cout << "NE" << endl;
+            }
+        }
     }
 
     return 0;
