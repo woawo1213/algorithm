475337d11b919266094c351dd46e00ea98737e3f
woawo1213 <woawo1213@gmail.com>
2022-03-05 14:02:10 +0900

    up

--- B_2559.cpp
+++ B_2559.cpp
@@ -1,22 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int n, k, temp, psum[100001], ret = -1000000;
+int seq, day, cnt[100];
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> temp;
-        psum[i] = psum[i - 1] + temp;
-    }
-    for (int i = k; i <= n; i++)
-    {
-        ret = max(ret, psum[i] - psum[i - k]);
-    }
-    cout << ret << "\n";
+
+    cin >> seq >> day;
+
     return 0;
 }
