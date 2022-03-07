9ee5cab7f8a146391a9dd1e4c12fa7acced75336
woawo1213 <woawo1213@gmail.com>
2022-03-07 23:51:19 +0900

    up

--- B_2559.cpp
+++ B_2559.cpp
@@ -1,6 +1,9 @@
+//구간합은 psum , psum[i]=psum[i-1]+a[i]
+//최대값은 최소값을 설정하고 최대값으로....
+//최소값은 최대값을 설정하고 최소값으로....
 #include <bits/stdc++.h>
 using namespace std;
-int seq, day, cnt[100];
+int seq, day, temp, psum[100001], ret = -10000004;
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -8,6 +11,16 @@ int main()
     cout.tie(NULL);
 
     cin >> seq >> day;
+    for (int i = 1; i <= seq; i++)
+    {
+        cin >> temp;
+        psum[i] = psum[i - 1] + temp;
+    }
+    for (int i = day; i <= seq; i++)
+    {
+        ret = max(ret, psum[i] - psum[i - day]);
+    }
+    cout << ret << endl;
 
     return 0;
 }
