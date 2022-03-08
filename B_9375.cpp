1a3e90bbad6454c431b5950de2e5ed2bccb70f6f
woawo1213 <woawo1213@gmail.com>
2022-03-08 13:24:17 +0900

    up

--- B_9375.cpp
+++ B_9375.cpp
@@ -1,22 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int t, n;
-string a, b;
-int main(){
+int n,t;
+string s1,s2;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> t;
-    while(t--){
-        map<string, int> _map;
+    while(t--)
+    {
+        map<string, int>_map;
         cin >> n;
-         for(int i = 0; i < n; i++){
-            cin >> a >> b;
-            _map[b]++;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> s1 >> s2;
+            _map[s2]++;
         }
         long long ret = 1;
-        for(auto c : _map){
-            ret *= ((long long)c.second + 1);
+        for(auto c:_map)
+        {
+            ret*=((long long)c.second+1);
         }
         ret--;
-        cout << ret << "\n";
+        cout<<ret<<endl;
     }
+
+    
+
+
+
+
     return 0;
 }
