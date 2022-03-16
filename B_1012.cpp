fb3f9697ba2c0b432ea4e02994e40ed7a230d8bc
woawo1213 <woawo1213@gmail.com>
2022-03-16 23:26:10 +0900

    up

--- B_1012.cpp
+++ B_1012.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int max_n = 104;
 int dy[4] = {-1, 0, 1, 0};
 int dx[4] = {0, 1, 0, -1};
-int t, n, m, k, x, y, ny, nx, ret;
+int m, n, k, y, x, ret, ny, nx, t;
 int a[51][51];
 bool visited[51][51];
 void dfs(int y, int x)
@@ -15,21 +14,19 @@ void dfs(int y, int x)
         nx = x + dx[i];
         if (ny < 0 || nx < 0 || ny >= n || nx >= m)
             continue;
-        if (a[ny][nx] == 1 && visited[ny][nx])
+        if (a[ny][nx] == 1 && !visited[ny][nx])
         {
             dfs(ny, nx);
         }
     }
     return;
 }
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
     cin >> t;
-
     while (t--)
     {
         fill(&a[0][0], &a[0][0] + 51 * 51, 0);
@@ -41,20 +38,18 @@ int main()
             cin >> x >> y;
             a[y][x] = 1;
         }
-
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                if (a[i][j] == 1 && visited[i][j])
+                if (a[i][j] == 1 && !visited[i][j])
                 {
                     dfs(i, j);
                     ret++;
                 }
             }
         }
-        cout << ret << endl;
+        cout << ret << "\n";
     }
-
     return 0;
 }
