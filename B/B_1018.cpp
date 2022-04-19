a05833dc466e81d2ee8a25c61c28175bb7d6fcae
woawo1213 <woawo1213@gmail.com>
2022-04-19 18:29:33 +0900

    up

---
+++ B/B_1018.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,cnt;
+vector<vector<string>> board;
+int main()
+{
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            string ss;
+            cin >> ss;
+            board[i].push_back(ss);
+        }
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            
+        }
+    }
+
+
+}
