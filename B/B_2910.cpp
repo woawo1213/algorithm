ad810b9f6c2aba395a4a4c8cc920bc984651811e
woawo1213 <woawo1213@gmail.com>
2022-04-28 21:54:44 +0900

    up

---
+++ B/B_2910.cpp
@@ -0,0 +1,18 @@
+//빈도 정렬
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+int n;
+ll c;
+vector<ll> freq;
+
+int main()
+{
+    cin >> n >> c;
+    for (int i = 0; i < n; i++)
+    {
+    }
+
+    return 0;
+}
---
+++ P/P_44.cpp
@@ -0,0 +1,11 @@
+//여행경로
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> solution(vector<vector<string>> tickets)
+{
+    vector<string> answer;
+
+    return answer;
+}
